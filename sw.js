31e2f151eb668bc880dcfdc59f323ea6c7aca1e4
Pandvas Solutions <contactat.rahulkumar@gmail.com>
2022-12-29 21:39:35 +0530

    pwa update

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e=> {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./","./src/master.css"],"./images/favicon/apple-touch-icon.png");
+            return cache.addAll(["./","./index.html","./css/style.css","./js/script.js","./images/logo/logo.png","./src/master.css"],);
         })
     )
 });
