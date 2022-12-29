a10a06dadf3e580137210af139c81c3a2050f356
Pandvas Solutions <contactat.rahulkumar@gmail.com>
2022-12-30 02:13:37 +0530

    minor change

--- js/script.js
+++ js/script.js
@@ -23,5 +23,3 @@ function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-
-
--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
-self.addEventListener("install", e=> {
+self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./","./index.html","./css/style.css","./js/script.js","./images/logo/logo.png","./images/icon/Search.png","./images/icon/submit.png","./src/master.css"],);
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./css/style.css",
+                "./js/script.js",
+                "./images/logo/logo.png",
+                "./images/icon/Search.png",
+                "./images/icon/submit.png",
+                "./src/master.css"
+            ], );
         })
     )
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(response => {
             return response || fetch(e.request);
